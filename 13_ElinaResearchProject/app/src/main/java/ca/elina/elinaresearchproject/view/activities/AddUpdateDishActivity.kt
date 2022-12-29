3bc0dad68683a55b52d02ef33714035a19396f27
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-12-29 13:10:56 -0500

    13. Glide intro

---
+++ 13_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,266 @@
+package ca.elina.elinaresearchproject.view.activities
+
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import ca.elina.elinaresearchproject.R
+import ca.elina.elinaresearchproject.databinding.ActivityAddUpdateDishBinding
+import ca.elina.elinaresearchproject.databinding.DialogCustomImageSelectionBinding
+import com.bumptech.glide.Glide
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+
+/**
+ * A screen where we can add and update the dishes.
+ */
+
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
+    }
+
+    override fun onClick(v: View) {
+
+        when (v.id) {
+
+            R.id.iv_add_dish_image -> {
+
+                customImageSelectionDialog()
+                return
+            }
+        }
+    }
+
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.  This follows the
+     * related Activity API as described there in
+     * {@link Activity#onActivityResult(int, int, Intent)}.
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+
+                data?.extras?.let {
+
+                    val thumbnail: Bitmap =
+                        data.extras!!.get("data") as Bitmap // Bitmap from camera
+                    // TODO Step 2: Here we will replace the setImageBitmap using glide as below.
+                    // START
+                    // mBinding.ivDishImage.setImageBitmap(thumbnail) // Set to the imageView.
+
+                    // Set Capture Image bitmap to the imageView using Glide
+                    Glide.with(this@AddUpdateDishActivity)
+                        .load(thumbnail)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
+                    // END
+
+                    // Replace the add icon with edit icon once the image is loaded.
+                    mBinding.ivAddDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@AddUpdateDishActivity,
+                            R.drawable.ic_vector_edit
+                        )
+                    )
+                }
+            }
+            // Get the selected image from gallery. The selected will be in form of URI so set it to the Dish ImageView.
+            else if (requestCode == GALLERY) {
+                data?.let {
+                    // Here we will get the select image URI.
+                    val selectedPhotoUri = data.data
+
+                    // TODO Step 3: Here we will replace the setImageURI using Glide as below.
+                    // START
+                    // mBinding.ivDishImage.setImageURI(selectedPhotoUri) // Set the selected image from GALLERY to imageView.
+
+                    // Set Selected Image bitmap to the imageView using Glide
+                    Glide.with(this@AddUpdateDishActivity)
+                        .load(selectedPhotoUri)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
+                    // END
+
+                    // Replace the add icon with edit icon once the image is selected.
+                    mBinding.ivAddDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@AddUpdateDishActivity,
+                            R.drawable.ic_vector_edit
+                        )
+                    )
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "User cancelled image selection.")
+        }
+    }
+
+    /**
+     * A function for ActionBar setup.
+     */
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+
+    /**
+     * A function to launch the custom image selection dialog.
+     */
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        // Here after all the permission are granted launch the CAMERA to capture an image.
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+
+                                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                                startActivityForResult(intent, CAMERA)
+                            }
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+
+            Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                        // Launch the gallery for Image selection using the constant.
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+
+                        startActivityForResult(galleryIntent, GALLERY)
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                        Toast.makeText(
+                            this@AddUpdateDishActivity,
+                            "You have denied the storage permission to select image.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permission: PermissionRequest,
+                        token: PermissionToken
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                })
+                .onSameThread()
+                .check()
+
+            dialog.dismiss()
+        }
+
+        //Start the dialog and display it on screen.
+        dialog.show()
+    }
+
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+
+    companion object {
+        // Add the constant for CAMERA.
+        private const val CAMERA = 1
+
+        // Add the constant for Gallery.
+        private const val GALLERY = 2
+    }
+}
