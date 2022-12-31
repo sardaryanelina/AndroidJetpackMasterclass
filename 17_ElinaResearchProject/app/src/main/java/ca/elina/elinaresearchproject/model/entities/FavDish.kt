855c58fa5a87eb03ac48ac301e57d686805a2f39
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-12-31 03:30:50 -0500

    17. Setup Room Database by adding all dependencies. Add a new entity fav_dishes_table.

---
+++ 17_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/model/entities/FavDish.kt
@@ -0,0 +1,24 @@
+package com.tutorials.eu.favdish.model.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// TODO Step 4: Create a new package name as entities in the model package. After creating the entities package create data class with the entities (Dish Items) that we want to insert in the database.
+// START
+// Define the Table name
+@Entity(tableName = "fav_dishes_table")
+data class FavDish(
+    @ColumnInfo val image: String,
+    @ColumnInfo val imageSource: String, // Local or Online
+    @ColumnInfo val title: String,
+    @ColumnInfo val type: String,
+    @ColumnInfo val category: String,
+    @ColumnInfo val ingredients: String,
+    // Specifies the name of the column in the table if you want it to be different from the name of the member variable.
+    @ColumnInfo(name = "cookingTime") val cooking_time: String,
+    @ColumnInfo(name = "instructions") val direction_to_cook: String,
+    @ColumnInfo(name = "favoriteDish") var favorite_dish: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
+// END
