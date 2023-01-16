a3a6df79d9320a641954897c2c0cedcf7a519d47
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-01-16 01:13:46 -0500

    35. Implement EDIT dish detail functionality using the popup menu and update the database’s dish details.

---
+++ 35_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/view/activities/MainActivity.kt
@@ -0,0 +1,61 @@
+package ca.elina.elinaresearchproject.view.activities
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import ca.elina.elinaresearchproject.R
+import ca.elina.elinaresearchproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mNavController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mNavController = findNavController(R.id.nav_host_fragment) // replace mNavController
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_all_dishes,
+                R.id.navigation_favorite_dishes,
+                R.id.navigation_random_dish
+            )
+        )
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+        mBinding.navView.setupWithNavController(mNavController) // use ViewBinding
+    }
+
+    // Override the onSupportNavigateUp method.
+    override fun onSupportNavigateUp(): Boolean {
+
+        // navigateUp with required parameters.
+        // This will navigate the user from DishDetailsFragment to AllDishesFragment
+        // when user clicks on the home back button.
+        return NavigationUI.navigateUp(mNavController, null) // user can navigate back
+    }
+
+
+    // A function to hide the BottomNavigationView with animation.
+    fun hideBottomNavigationView() {
+        mBinding.navView.clearAnimation() // in case there are animations running, we get rid of them
+        mBinding.navView.animate().translationY(mBinding.navView.height.toFloat()).duration = 300
+    }
+
+    // A function to show the BottomNavigationView with Animation.
+    fun showBottomNavigationView() {
+        mBinding.navView.clearAnimation()
+        mBinding.navView.animate().translationY(0f).duration = 300
+    }
+}
