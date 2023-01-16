9e9b2268e04779184523424e6960f3cb81dfbbd3
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-01-16 00:57:52 -0500

    34. Design and implement the popup menu to EDIT and DELETE the dish item. For now, the selection will be printed in the Log.

---
+++ 34_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/view/adapters/FavDishAdapter.kt
@@ -0,0 +1,122 @@
+package ca.elina.elinaresearchproject.view.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import ca.elina.elinaresearchproject.R
+import ca.elina.elinaresearchproject.databinding.ItemDishLayoutBinding
+import ca.elina.elinaresearchproject.model.entities.FavDish
+import ca.elina.elinaresearchproject.view.fragments.AllDishesFragment
+import ca.elina.elinaresearchproject.view.fragments.FavoriteDishesFragment
+import com.bumptech.glide.Glide
+
+class FavDishAdapter(private val fragment: Fragment) :
+    RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
+
+    private var dishes: List<FavDish> = listOf()
+
+    /**
+     * Inflates the item views which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemDishLayoutBinding =
+            ItemDishLayoutBinding.inflate(LayoutInflater.from(fragment.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val dish = dishes[position]
+
+        // Load the dish image in the ImageView.
+        Glide.with(fragment)
+            .load(dish.image)
+            .into(holder.ivDishImage)
+
+        holder.tvTitle.text = dish.title
+
+        holder.itemView.setOnClickListener {
+            if (fragment is AllDishesFragment) {
+                fragment.dishDetails(dish) // Pass the required param value.
+                // Call the function to navigate the item to Details Fragment.
+            } else if (fragment is FavoriteDishesFragment) {
+                fragment.dishDetails(dish)
+            }
+        }
+
+        // TODO Step 7: We want the menu icon should be visible only in the AllDishesFragment not in the FavoriteDishesFragment so add the below to achieve it.
+        // START
+        if (fragment is AllDishesFragment) {
+            holder.ibMore.visibility = View.VISIBLE
+        } else if (fragment is FavoriteDishesFragment) {
+            holder.ibMore.visibility = View.GONE
+        }
+        // END
+
+        // TODO Step 6: Assign the click event to the ib_more icon and Popup the menu items.
+        // START
+        holder.ibMore.setOnClickListener {
+            val popup = PopupMenu(fragment.context, holder.ibMore)
+            //Inflating the Popup using xml file
+            popup.menuInflater.inflate(R.menu.menu_adapter, popup.menu)
+
+            // TODO Step 8: Assign the click event to the menu items as below and print the Log or You can display the Toast message for now.
+            // START
+            popup.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    Log.i("You have clicked on", "Edit Option of ${dish.title}")
+                } else if (it.itemId == R.id.action_delete_dish) {
+                    Log.i("You have clicked on", "Delete Option of ${dish.title}")
+                }
+                true
+            }
+            // END
+
+            popup.show() //showing popup menu
+        }
+        // END
+    }
+
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return dishes.size
+    }
+
+    fun dishesList(list: List<FavDish>) {
+        dishes = list
+        notifyDataSetChanged()
+    }
+
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+        // Holds the TextView that will add each item to
+        val ivDishImage = view.ivDishImage
+        val tvTitle = view.tvDishTitle
+
+        // TODO Step 5: Create a variable for more menu icon.
+        // START
+        val ibMore = view.ibMore
+        // END
+    }
+}
