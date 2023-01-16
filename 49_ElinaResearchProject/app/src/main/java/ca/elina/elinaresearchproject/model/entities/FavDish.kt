a28da3984f47951418480d52f05eea0b13ae4a2c
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2023-01-16 12:00:35 -0500

    49. Add the feature to share the dish details using Sharing.

---
+++ 49_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/model/entities/FavDish.kt
@@ -0,0 +1,30 @@
+package ca.elina.elinaresearchproject.model.entities
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+
+// Annotate a class with @Parcelize and implement Parcelable.
+@Parcelize
+// Create a new package name as entities in the model package. After creating the entities package
+// create data class with the entities (Dish Items) that we want to insert in the database.
+// Define the Table name
+@Entity(tableName = "fav_dishes_table")
+data class FavDish(
+    @ColumnInfo val image: String,
+    @ColumnInfo(name = "image_source") val imageSource: String, // Local or Online
+    @ColumnInfo val title: String,
+    @ColumnInfo val type: String,
+    @ColumnInfo val category: String,
+    @ColumnInfo val ingredients: String,
+    // Specifies the name of the column in the table if you want it to be different from the name of the member variable.
+    @ColumnInfo(name = "cooking_time") val cookingTime: String,
+    @ColumnInfo(name = "instructions") val directionToCook: String,
+    @ColumnInfo(name = "favorite_dish") var favoriteDish: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable
+// Parcelable is similar to Serializable.
+// It makes an object into a String that can easily pass from one screen to another.
---
+++ 49_ElinaResearchProject/app/src/main/java/ca/elina/elinaresearchproject/view/fragments/DishDetailsFragment.kt
@@ -0,0 +1,266 @@
+package ca.elina.elinaresearchproject.view.fragments
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.graphics.drawable.toBitmap
+import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import java.io.IOException
+import java.util.*
+import androidx.annotation.Nullable
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.request.target.Target
+import ca.elina.elinaresearchproject.R
+import ca.elina.elinaresearchproject.application.FavDishApplication
+import ca.elina.elinaresearchproject.databinding.FragmentDishDetailsBinding
+import ca.elina.elinaresearchproject.model.entities.FavDish
+import ca.elina.elinaresearchproject.utils.Constants
+import ca.elina.elinaresearchproject.viewmodel.FavDishViewModel
+import ca.elina.elinaresearchproject.viewmodel.FavDishViewModelFactory
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DishDetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DishDetailsFragment : Fragment() {
+
+    private var mBinding: FragmentDishDetailsBinding? = null
+
+    /**
+     * To create the ViewModel we used the viewModels delegate, passing in an instance of our FavDishViewModelFactory.
+     * This is constructed based on the repository retrieved from the FavDishApplication.
+     */
+    private val mFavDishViewModel: FavDishViewModel by viewModels {
+        FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+    }
+
+    // TODO Step 6: Create a global variable  for Dish Details and assign the args to it.
+    // START
+    private var mFavDishDetails: FavDish? = null
+    // END
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO Step 4: Set the setHasOptionsMenu to true.
+        // START
+        setHasOptionsMenu(true)
+        // END
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        mBinding = FragmentDishDetailsBinding.inflate(inflater, container, false)
+        return mBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args: DishDetailsFragmentArgs by navArgs()
+
+        // TODO Step 7: Initialize the FavDish global variable.
+        // START
+        mFavDishDetails = args.dishDetails
+        // END
+
+        args.let {
+
+            try {
+                // Load the dish image in the ImageView.
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            @Nullable e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            // log exception
+                            Log.e("TAG", "Error loading image", e)
+                            return false // important to return false so the error placeholder can be placed
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+
+                            Palette.from(resource.toBitmap())
+                                .generate { palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                    mBinding!!.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            return false
+                        }
+                    })
+                    .into(mBinding!!.ivDishImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            mBinding!!.tvTitle.text = it.dishDetails.title
+            mBinding!!.tvType.text =
+                it.dishDetails.type.capitalize(Locale.ROOT) // Used to make first letter capital
+            mBinding!!.tvCategory.text = it.dishDetails.category
+            mBinding!!.tvIngredients.text = it.dishDetails.ingredients
+
+            // TODO Step 9: Load the HTML text to TextView.
+            // START
+            // The instruction or you can say the Cooking direction text is in the HTML format so we will you the fromHtml to populate it in the TextView.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                mBinding!!.tvCookingDirection.text = Html.fromHtml(
+                    it.dishDetails.directionToCook,
+                    Html.FROM_HTML_MODE_COMPACT
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                mBinding!!.tvCookingDirection.text = Html.fromHtml(it.dishDetails.directionToCook)
+            }
+            // END
+
+            mBinding!!.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+
+            if (args.dishDetails.favoriteDish) {
+                mBinding!!.ivFavoriteDish.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_selected
+                    )
+                )
+            } else {
+                mBinding!!.ivFavoriteDish.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_unselected
+                    )
+                )
+            }
+        }
+
+        mBinding!!.ivFavoriteDish.setOnClickListener {
+
+            args.dishDetails.favoriteDish = !args.dishDetails.favoriteDish
+
+            mFavDishViewModel.update(args.dishDetails)
+
+            if (args.dishDetails.favoriteDish) {
+                mBinding!!.ivFavoriteDish.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_selected
+                    )
+                )
+
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_added_to_favorites),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                mBinding!!.ivFavoriteDish.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_unselected
+                    )
+                )
+
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_removed_from_favorite),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    // TODO Step 5: Override the onCreateOptionsMenu and onOptionsItemSelected. Inflate the menu file that we have created.
+    // START
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_share, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            // TODO Step 8: Handle Item click action and share the dish recipe details with others.
+            // START
+            R.id.action_share_dish -> {
+
+                val type = "text/plain"
+                val subject = "Checkout this dish recipe"
+                var extraText = ""
+                val shareWith = "Share with"
+
+                mFavDishDetails?.let {
+
+                    var image = ""
+
+                    if (it.imageSource == Constants.DISH_IMAGE_SOURCE_ONLINE) {
+                        image = it.image
+                    }
+
+                    var cookingInstructions = ""
+
+                    // The instruction or you can say the Cooking direction text is in the HTML format so we will you the fromHtml to populate it in the TextView.
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        cookingInstructions = Html.fromHtml(
+                            it.directionToCook,
+                            Html.FROM_HTML_MODE_COMPACT
+                        ).toString()
+                    } else {
+                        @Suppress("DEPRECATION")
+                        cookingInstructions = Html.fromHtml(it.directionToCook).toString()
+                    }
+
+                    extraText =
+                        "$image \n" +
+                                "\n Title:  ${it.title} \n\n Type: ${it.type} \n\n Category: ${it.category}" +
+                                "\n\n Ingredients: \n ${it.ingredients} \n\n Instructions To Cook: \n $cookingInstructions" +
+                                "\n\n Time required to cook the dish approx ${it.cookingTime} minutes."
+                }
+
+
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = type
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                intent.putExtra(Intent.EXTRA_TEXT, extraText)
+                startActivity(Intent.createChooser(intent, shareWith))
+
+                return true
+            }
+            // END
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    // END
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
+    }
+}
